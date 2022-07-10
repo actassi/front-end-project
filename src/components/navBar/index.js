4242e0a75ff5fd727a12ce6359fe4761f054ab07
Sebastian Eugenio <sebas-cs1.6@hotmail.com>
2022-07-10 15:32:33 -0300

    fix url

--- src/components/navBar/index.js
+++ src/components/navBar/index.js
@@ -2,16 +2,9 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
 import Logo from '../logo';
 import { Stack } from '@mui/material';
 import { ColorModeContext } from '../../context/colorModeContext';
@@ -20,16 +13,11 @@ import { useNavigate } from 'react-router-dom';
 
 
 const NavBar = () => {
-	const [anchorElNav, setAnchorElNav] = React.useState(null);
-	const [anchorElUser, setAnchorElUser] = React.useState(null);
 	const { mode, toggleColorMode } = React.useContext(ColorModeContext);
-	const [isLoged, setLoged] = React.useState(true);
+	const [isLoged] = React.useState(true);
 	const { logout, user } = useAuth();
 	const navigate = useNavigate()
 
-	const handleOpenNavMenu = (event) => {
-		setAnchorElNav(event.currentTarget);
-	};
 	const handleDarkMode = () => {
 		toggleColorMode();
 	};
@@ -40,6 +28,9 @@ const NavBar = () => {
 
 	const handleHome = () => {
 		navigate('/')
+	}
+	const handleFixture = () => {
+		navigate('/fixture')
 	}
 	return (
 		(user) && <AppBar position="absolute" sx={{ bgcolor: "background.default" }}>
@@ -64,7 +55,7 @@ const NavBar = () => {
 					</Box>
 					<Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
 						<Button onClick={handleHome} color="primary">Home</Button>
-						<Button color="primary">Fixture</Button>
+						<Button onClick={handleFixture} color="primary">Fixture</Button>
 					</Box>
 					<Stack direction={"row"} alignItems="center">
 
